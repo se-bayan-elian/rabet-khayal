3b4b2a5dd8ed70de0336f4dfa894bb341a0649c5
Bayan Elian <bezo2024@gmail.com>
2025-09-29 16:23:03 +0300

    style : remove classes from outline buttons in about us page

--- components/pages/AboutPage.tsx
+++ components/pages/AboutPage.tsx
@@ -143,7 +143,7 @@ export default function AboutPage({ locale }: AboutPageProps) {
                   {t('hero.cta')}
                   <ArrowRight className="w-5 h-5 ml-2" />
                 </Button>
-                <Button size="lg" variant="outline" className="border-white text-white hover:bg-white hover:text-brand-navy">
+                <Button size="lg" variant="outline" >
                   {t('hero.portfolio')}
                 </Button>
               </div>
@@ -375,7 +375,7 @@ export default function AboutPage({ locale }: AboutPageProps) {
               <Mail className="w-5 h-5 mr-2" />
               {t('contact.email')}
             </Button>
-            <Button size="lg" variant="outline" className="border-white text-white hover:bg-white hover:text-brand-navy">
+            <Button size="lg" variant="outline" >
               <Phone className="w-5 h-5 mr-2" />
               {t('contact.phone')}
             </Button>
